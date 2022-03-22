8ed9f62b2152577feef76a6457b460dd6396948a
Temidayo <temideewan@gmail.com>
2022-03-22 14:10:19 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL302_SR302,200_.jpg",
+    title: "I love you to the moon and back",
+    author: "Adam Wallace",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/91WXnGm5sUL._AC_UL906_SR906,600_.jpg",
+    title: "Disney Encanto Little Golden Book (Disney Encanto)",
+    author: "Naibe Reynoso",
+  },
+  {
+    id: 3,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51v0gkGIaKL._AC_UL906_SR906,600_.jpg",
+    title: "Harry Potter Paperback Box Set (Books 1-7)",
+    author: "J.K. Rowling",
+  },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Booklist = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book key={book.id} {...book} />;
+      })}
+    </section>
+  );
+};
+
+const Book = (props) => {
+  console.log(props);
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt="catch a leprechaun" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+ReactDOM.render(<Booklist />, document.getElementById("root"));
